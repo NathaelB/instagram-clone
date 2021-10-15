b02cbcab4ab42414192cb760fd6cbf32c71b52aa
Nathael Bonnal <kirua48000@gmail.com>
2021-10-15 15:22:15 +0200

    :construction: add <Feed /> component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import Head from 'next/head'
 import Header from '../components/Header'
+import Feed from '../components/Feed'
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="overflow-x-hidden bg-gray-50 h-screen scrollbar-hide overflow-y-scroll">
 
       <Header />
+      <Feed />
     </div>
   )
 }
