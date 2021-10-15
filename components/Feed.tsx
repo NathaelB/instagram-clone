8628e4770e53b80bb233c18fc35bebcf97df5576
Nathael Bonnal <kirua48000@gmail.com>
2021-10-15 15:21:40 +0200

    :construction: create add all components

---
+++ components/Feed.tsx
@@ -0,0 +1,26 @@
+import Stories from './Stories'
+import Posts from './Posts'
+
+
+function Feed () {
+  return (
+    <main className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto">
+      {/* Section */}
+      <section className="col-span-2">
+        <Stories />
+        <Posts />
+      </section>
+        {/* Stories */}
+        {/* Posts */}
+
+      {/* Section */}
+      <section>
+        test
+      </section>
+        {/* Mini Profil */}
+        {/* Suggestions */}
+    </main>
+  )
+}
+
+export default Feed
